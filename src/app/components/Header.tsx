b5f4ba55f242d4eb022d13d05c2daa671a2b4cd2
Emerson Fernadez <escanorrosa123@gmail.com>
2025-07-24 21:46:24 -0500

    sasa

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useCallback, useEffect, useState } from "react";
+import { TiShoppingCart } from "react-icons/ti";
 
 export default function Header() {
   const [openMenu, setOpenMenu] = useState(false);
@@ -14,7 +15,7 @@ export default function Header() {
 
   useEffect(() => {
     const handleScroll = () => {
-      setIsScrolled(window.scrollY > 500);
+      setIsScrolled(window.scrollY > 300);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -23,32 +24,35 @@ export default function Header() {
 
   return (
     <header
-      className={`fixed top-0 w-screen px-10 z-[100] font-[SegoeUiBlack] text-white transition-colors duration-300 ${
-        isScrolled ? "bg-blue" : "bg-transparent"
+      className={`fixed top-0 w-screen px-10 z-[100] font-[SegoeUiBlack] text-white transition-colors duration-[0.7s] ${
+        isScrolled ? "bg-blue shadow-lg shadow-[#0000002b]" : "bg-transparent"
       }`}
     >
       <div className="w-full max-w-[1400px] m-auto flex justify-between items-center">
         <Link href="/" onClick={() => setOpenMenu(false)}>
           <Image
-            className="w-30"
+            className="w-26"
             src="/identidadVisual/Logo-Regipets-blanco-01.png"
             alt="Banner de inicio"
             width={800}
             height={800}
           />
         </Link>
-        <div className="pr-10">
+        <div className="flex gap-4 pr-10">
+          <button className="relative w-14 h-12 bg-orange flex flex-col items-center justify-center gap-2 rounded-3xl cursor-pointer z-20">
+            <TiShoppingCart className="text-white text-4xl" />
+          </button>
           <button
-            className="relative w-14 h-12 bg-blue flex flex-col items-center justify-center gap-2 rounded-3xl cursor-pointer z-20"
+            className="relative w-14 h-12 bg-white flex flex-col items-center justify-center gap-2 rounded-3xl cursor-pointer z-20"
             onClick={toggleMenu}
           >
             <span
-              className={`block w-7 h-1 rounded-lg bg-white ${
+              className={`block w-7 h-1 rounded-lg bg-blue ${
                 openMenu ? "rotate-45 absolute" : ""
               } duration-250`}
             ></span>
             <span
-              className={`block w-7 h-1 rounded-lg bg-white ${
+              className={`block w-7 h-1 rounded-lg bg-blue ${
                 openMenu ? "-rotate-45 absolute" : ""
               } duration-250`}
             ></span>
@@ -56,8 +60,8 @@ export default function Header() {
         </div>
 
         <nav
-          className={`fixed z-10 top-0 right-0 h-screen w-[340px] bg-blue pt-[150px] flex flex-col items-end gap-4 pr-16 text-xl ${
-            openMenu ? "" : "translate-x-full"
+          className={`fixed   z-10 top-0 right-0 h-screen w-[340px] bg-blue pt-[150px] flex flex-col items-end gap-4 pr-16 text-xl ${
+            openMenu ? " box-shadowMenu" : "translate-x-full "
           } duration-250`}
         >
           <Link href="/quienes-somos" onClick={() => setOpenMenu(false)}>
